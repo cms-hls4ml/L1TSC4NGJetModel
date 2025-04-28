a13fbd9fa7c5b33d10efff260fb288cdd141b137
Christopher Brown <chris.brown@fpsl.net>
2025-04-28 18:59:44 +0200

    Update setup.sh to work with new model name

--- setup.sh
+++ setup.sh
@@ -1,31 +1,33 @@
-mkdir -p L1TSC4NGJetModel/NN
-SRC=L1TSC4NGJetModel/firmware
+TARGET=L1TSC4NGJetModel_v0/NN
+mkdir -p $TARGET
+SRC=L1TSC4NGJetModel
 
-cp -r $SRC/defines.h   L1TSC4NGJetModel/NN/
-sed -i "s/#include <cstdio>/#include <cstdio>\n\nnamespace hls4ml_L1TSC4NGJetModel_v0 {/" L1TSC4NGJetModel/NN/defines.h
-echo '}' >> L1TSC4NGJetModel/NN/defines.h
+cp -r $SRC/defines.h ${TARGET}
+sed -i .tmp "s/#include <cstdio>/#include <cstdio>\n\nnamespace hls4ml_L1TSC4NGJetModel_v0 {/" ${TARGET}/defines.h
+echo '}' >> ${TARGET}/defines.h
 
-cp    $SRC/L1TSC4NGJetModel.cpp   L1TSC4NGJetModel/NN/L1TSC4NGJetModel.cpp
-sed -i "s/void L1TSC4NGJetModel(/namespace hls4ml_L1TSC4NGJetModel_v0 {\nvoid L1TSC4NGJetModel(/" L1TSC4NGJetModel/NN/L1TSC4NGJetModel.cpp
-echo '}' >> L1TSC4NGJetModel/NN/L1TSC4NGJetModel.cpp
+cp $SRC/L1TSC4NGJetModel.cpp   ${TARGET}/L1TSC4NGJetModel_v0.cpp
+sed -i .tmp "s/void L1TSC4NGJetModel(/namespace hls4ml_L1TSC4NGJetModel_v0 {\nvoid L1TSC4NGJetModel(/" ${TARGET}/L1TSC4NGJetModel_v0.cpp
+echo '}' >> ${TARGET}/L1TSC4NGJetModel_v0.cpp
 
-cp    $SRC/L1TSC4NGJetModel.h   L1TSC4NGJetModel/NN/L1TSC4NGJetModel.h
-sed -i "s/void L1TSC4NGJetModel(/namespace hls4ml_L1TSC4NGJetModel_v0 {\nvoid L1TSC4NGJetModel(/" L1TSC4NGJetModel/NN/L1TSC4NGJetModel.h
-sed -i "s/#endif/}\n\n#endif/" L1TSC4NGJetModel/NN/L1TSC4NGJetModel.h
+cp    $SRC/L1TSC4NGJetModel.h   ${TARGET}/L1TSC4NGJetModel_v0.h
+sed -i .tmp "s/void L1TSC4NGJetModel(/namespace hls4ml_L1TSC4NGJetModel_v0 {\nvoid L1TSC4NGJetModel(/" ${TARGET}/L1TSC4NGJetModel_v0.h
+sed -i .tmp "s/#endif/}\n\n#endif/" ${TARGET}/L1TSC4NGJetModel_v0.h
 
-cp -r $SRC/nnet_utils   L1TSC4NGJetModel/NN/
+cp -r $SRC/nnet_utils ${TARGET}
 
-cp    $SRC/parameters.h   L1TSC4NGJetModel/NN/
-sed -i "s,// hls-fpga-machine-learning insert layer-config,namespace hls4ml_L1TSC4NGJetModel_v0 {\n// hls-fpga-machine-learning insert layer-config," L1TSC4NGJetModel/NN/parameters.h
-sed -i "s/#endif/}\n\n#endif/" L1TSC4NGJetModel/NN/parameters.h
+cp    $SRC/parameters.h ${TARGET}
+sed -i .tmp "s,// hls-fpga-machine-learning insert layer-config,namespace hls4ml_L1TSC4NGJetModel_v0 {\n// hls-fpga-machine-learning insert layer-config," ${TARGET}/parameters.h
+sed -i .tmp "s/#endif/}\n\n#endif/" ${TARGET}/parameters.h
 
-cp -r $SRC/weights    L1TSC4NGJetModel/NN/
-for filename in L1TSC4NGJetModel/NN/weights/*.h; do
+cp -r $SRC/weights ${TARGET}
+for filename in ${TARGET}/weights/*.h; do
+    echo $filename
     sed -i '8 a namespace hls4ml_L1TSC4NGJetModel_v0 {' $filename
     echo '}' >> $filename
 done
 
-rm L1TSC4NGJetModel/NN/weights/*.txt
+rm ${TARGET}/weights/*.txt
 
 # change the way the weights are read
-find ./L1TSC4NGJetModel/NN \( -type d -name .git -prune \) -o -type f -print0 | xargs -0 sed -i 's/#ifndef __SYNTHESIS__/#ifdef __HLS4ML_LOAD_TXT_WEIGHTS__/'
+find ./${TARGET} \( -type d -name .git -prune \) -o -type f -print0 | xargs -0 sed -i 's/#ifndef __SYNTHESIS__/#ifdef __HLS4ML_LOAD_TXT_WEIGHTS__/'
