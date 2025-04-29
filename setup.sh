8da8a1c4f9b49b3efef29de5df763f1d68ee9b7a
Christopher Brown <chris.brown@fpsl.net>
2025-04-29 12:08:38 +0200

    Copy proper filepath to target in setup

--- setup.sh
+++ setup.sh
@@ -7,11 +7,11 @@ cp -r $SRC/defines.h ${TARGET}
 sed -i "s/#include <cstdio>/#include <cstdio>\n\nnamespace hls4ml_L1TSC4NGJetModel_$VERSION {/" ${TARGET}/defines.h
 echo '}' >> ${TARGET}/defines.h
 
-cp $SRC/L1TSC4NGJetModel.cpp   ${TARGET}/L1TSC4NGJetModel_$VERSION.cpp
+cp $SRC/L1TSC4NGJetModel_$VERSION.cpp   ${TARGET}/L1TSC4NGJetModel_$VERSION.cpp
 sed -i "s/void L1TSC4NGJetModel(/namespace hls4ml_L1TSC4NGJetModel_$VERSION {\nvoid L1TSC4NGJetModel(/" ${TARGET}/L1TSC4NGJetModel_$VERSION.cpp
 echo '}' >> ${TARGET}/L1TSC4NGJetModel_$VERSION.cpp
 
-cp    $SRC/L1TSC4NGJetModel.h   ${TARGET}/L1TSC4NGJetModel_$VERSION.h
+cp    $SRC/L1TSC4NGJetModel_$VERSION.h   ${TARGET}/L1TSC4NGJetModel_$VERSION.h
 sed -i "s/void L1TSC4NGJetModel(/namespace hls4ml_L1TSC4NGJetModel_$VERSION {\nvoid L1TSC4NGJetModel(/" ${TARGET}/L1TSC4NGJetModel_$VERSION.h
 sed -i "s/#endif/}\n\n#endif/" ${TARGET}/L1TSC4NGJetModel_$VERSION.h
 
