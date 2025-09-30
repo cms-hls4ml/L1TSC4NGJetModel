8bbb1846852d38911efb36923a4f2ff56a820b2b
Christopher Brown <chris.brown@fpsl.net>
2025-09-30 11:05:19 +0200

    Change n layers for classification as well?

--- L1TSC4NGJetModel_test/L1TSC4NGJetModel_emulator_test.cpp
+++ L1TSC4NGJetModel_test/L1TSC4NGJetModel_emulator_test.cpp
@@ -12,7 +12,7 @@ using namespace hls4ml_L1TSC4NGJetModel_test;
 class L1TSC4NGJetModel_emulator_test : public hls4mlEmulator::Model{
     private:
         input_t _input[N_INPUT_1_1*N_INPUT_2_1];
-        layer24_t _layer24_out[N_LAYER_23]; // reg out
+        layer24_t _layer24_out[N_LAYER_20]; // reg out
         layer25_t _layer25_out[N_LAYER_22]; // class out
     public:
 
@@ -35,8 +35,8 @@ class L1TSC4NGJetModel_emulator_test : public hls4mlEmulator::Model{
 
         virtual void read_result(std::any result)
         { 
-            std::pair<std::array<layer24_t,N_LAYER_23>,std::array<layer25_t,N_LAYER_22>> *result_p = std::any_cast<std::pair<std::array<layer24_t,N_LAYER_23>,std::array<layer25_t,N_LAYER_22>>*>(result);
-            for (int i = 0; i < N_LAYER_23; ++i ){
+            std::pair<std::array<layer24_t,N_LAYER_20>,std::array<layer25_t,N_LAYER_22>> *result_p = std::any_cast<std::pair<std::array<layer24_t,N_LAYER_20>,std::array<layer25_t,N_LAYER_22>>*>(result);
+            for (int i = 0; i < N_LAYER_20; ++i ){
                 result_p->first[i] = _layer24_out[i];  
             }
             for (int i = 0; i < N_LAYER_22; ++i ){
