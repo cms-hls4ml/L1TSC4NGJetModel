f09b2f0c1972a8ac70431b567817bb0b0189390f
Christopher Brown <chris.brown@fpsl.net>
2025-09-30 10:59:00 +0200

    Add extra layer at end of regression

--- L1TSC4NGJetModel_test/L1TSC4NGJetModel_emulator_test.cpp
+++ L1TSC4NGJetModel_test/L1TSC4NGJetModel_emulator_test.cpp
@@ -13,7 +13,7 @@ class L1TSC4NGJetModel_emulator_test : public hls4mlEmulator::Model{
     private:
         input_t _input[N_INPUT_1_1*N_INPUT_2_1];
         layer24_t _layer24_out[N_LAYER_23]; // reg out
-        layer22_t _layer22_out[N_LAYER_19]; // class out
+        layer25_t _layer25_out[N_LAYER_22]; // class out
     public:
 
 
@@ -29,18 +29,18 @@ class L1TSC4NGJetModel_emulator_test : public hls4mlEmulator::Model{
 
         virtual void predict()
         {
-            L1TSC4NGJetModel_test(_input, _layer22_out, _layer24_out);
+            L1TSC4NGJetModel_test(_input, _layer25_out, _layer24_out);
             
         }
 
         virtual void read_result(std::any result)
         { 
-            std::pair<std::array<layer24_t,N_LAYER_23>,std::array<layer22_t,N_LAYER_19>> *result_p = std::any_cast<std::pair<std::array<layer24_t,N_LAYER_23>,std::array<layer22_t,N_LAYER_19>>*>(result);
+            std::pair<std::array<layer24_t,N_LAYER_23>,std::array<layer25_t,N_LAYER_22>> *result_p = std::any_cast<std::pair<std::array<layer24_t,N_LAYER_23>,std::array<layer25_t,N_LAYER_22>>*>(result);
             for (int i = 0; i < N_LAYER_23; ++i ){
                 result_p->first[i] = _layer24_out[i];  
             }
-            for (int i = 0; i < N_LAYER_19; ++i ){
-                result_p->second[i] = _layer22_out[i];
+            for (int i = 0; i < N_LAYER_22; ++i ){
+                result_p->second[i] = _layer25_out[i];
             }
         }
 
