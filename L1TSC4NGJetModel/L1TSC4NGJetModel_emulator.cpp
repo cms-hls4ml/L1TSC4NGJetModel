104a4aa11e3ffd62e18f83ed7dec6145de0cc11b
Stella <stella.felice.schaefer@desy.de>
2026-04-15 16:11:16 +0200

    changes to include pile-up class nd multiple inputs

--- L1TSC4NGJetModel/L1TSC4NGJetModel_emulator.cpp
+++ L1TSC4NGJetModel/L1TSC4NGJetModel_emulator.cpp
@@ -3,44 +3,68 @@
 #include "NN/nnet_utils/nnet_common.h"
 #include <any>
 #include <array>
+#include <codecvt>
+#include <iostream>
 #include <utility>
 #include "ap_fixed.h"
 #include "ap_int.h"
 
 using namespace hls4ml_L1TSC4NGJetModel;
 
+struct ModelInputs {
+    input_t* basic_input;
+    input32_t* constituent_fraction;
+    input12_t* jet_features;
+    input14_t* pt_mask;
+};
+
 class L1TSC4NGJetModel_emulator : public hls4mlEmulator::Model{
     private:
-        input_t _input[N_INPUT_1_1*N_INPUT_2_1];
-        layer24_t _layer24_out[N_LAYER_23]; // reg out
-        layer22_t _layer22_out[N_LAYER_19]; // class out
+        input_t _input0[16*21];
+        input32_t _input1[16];
+        input12_t _input2[2];
+        input14_t _input3[16];
+        layer39_t _layer39_out[9]; // class out
+        layer40_t _layer40_out[1]; // reg out
     public:
 
+        virtual void prepare_input(std::any input) override {
+            auto inputs = std::any_cast<ModelInputs>(input);
 
-        virtual void prepare_input(std::any input)
-        {
-            input_t* input_p = std::any_cast<input_t*>(input);
-            for(int i = 0; i < N_INPUT_1_1*N_INPUT_2_1; ++i){
-                _input[i] = std::any_cast<input_t>(input_p[i]);
+            // Input NN
+            for (int i = 0; i < 16*21; ++i) {
+                _input0[i] = inputs.basic_input[i];
+            }
+
+            // Constituent Fraction
+            for (int i = 0; i < 16; ++i) {
+                _input1[i] = inputs.constituent_fraction[i];
+            }
+
+            // Jet Features
+            for (int i = 0; i < 2; ++i) {
+                _input2[i] = inputs.jet_features[i];
+            }
+
+            // Pt Mask
+            for (int i = 0; i < 16; ++i) {
+                _input3[i] = inputs.pt_mask[i];
             }
         }
-
-
 
         virtual void predict()
         {
-            L1TSC4NGJetModel(_input, _layer22_out, _layer24_out);
-            
+            L1TSC4NGJetModel(_input0, _input1, _input2, _input3, _layer39_out, _layer40_out);
         }
 
         virtual void read_result(std::any result)
-        { 
-            std::pair<std::array<layer24_t,N_LAYER_23>,std::array<layer22_t,N_LAYER_19>> *result_p = std::any_cast<std::pair<std::array<layer24_t,N_LAYER_23>,std::array<layer22_t,N_LAYER_19>>*>(result);
-            for (int i = 0; i < N_LAYER_23; ++i ){
-                result_p->first[i] = _layer24_out[i];  
+        {
+            std::pair<std::array<layer40_t,1>,std::array<layer39_t,9>> *result_p = std::any_cast<std::pair<std::array<layer40_t,1>,std::array<layer39_t,9>>*>(result);
+            for (int i = 0; i < 1; ++i ){
+                result_p->first[i] = _layer40_out[i];
             }
-            for (int i = 0; i < N_LAYER_19; ++i ){
-                result_p->second[i] = _layer22_out[i];
+            for (int i = 0; i < 9; ++i ){
+                result_p->second[i] = _layer39_out[i];
             }
         }
 
