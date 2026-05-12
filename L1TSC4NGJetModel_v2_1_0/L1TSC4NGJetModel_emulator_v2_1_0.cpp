c20d4e14e05c35a54688c52efa3f83f4b0a292cf
Christopher <cb719@ic.ac.uk>
2026-05-12 16:17:34 +0200

    update setup script to properly deal with v2 and v2_1 models

--- L1TSC4NGJetModel_v2_1_0/L1TSC4NGJetModel_emulator_v2_1_0.cpp
+++ L1TSC4NGJetModel_v2_1_0/L1TSC4NGJetModel_emulator_v2_1_0.cpp
@@ -25,12 +25,12 @@ struct ModelOutputs {
 
 class L1TSC4NGJetModel_emulator_v2_1_0 : public hls4mlEmulator::Model{
     private:
-        input_t _input0[16*21];
-        input32_t _input1[16];
-        input12_t _input2[2];
-        input14_t _input3[16];
-        layer39_t _layer39_out[9]; // class out
-        layer40_t _layer40_out[1]; // reg out
+        input_t basic_input[16*21];
+        input32_t constituent_fraction[16];
+        input12_t jet_features[2];
+        input14_t pt_mask[16];
+        layer39_t class_out[9]; // class out
+        layer40_t reg_out[1]; // reg out
     public:
 
 
@@ -39,28 +39,29 @@ class L1TSC4NGJetModel_emulator_v2_1_0 : public hls4mlEmulator::Model{
             // Basic inputs 16*20  for baseline inputs
             for (int i = 0; i < 16; ++i) { // Iterate through candidates
                 for (int j = 0; j < 21; ++j) { // Iterate through features
-                    _input_basic[i*N_INPUT_2_1 + j] = input_t(inputs.candidate_inputs[i*inputs.total_candidate_inputs + j] );
+                    basic_input[i*21 + j] = input_t(inputs.candidate_inputs[i*inputs.total_candidate_inputs + j] );
                 }
             }
 
             // Fraction inputs
             for (int i = 0; i < 16; ++i) { // Iterate through features
-                _input_fractions[i] = input32_t(inputs.candidate_inputs[1 + inputs.total_candidate_inputs*i]);
+                constituent_fraction[i] = input32_t(inputs.candidate_inputs[1 + inputs.total_candidate_inputs*i]);
             }
 
             // Mask inputs
             for (int i = 0; i < 16; ++i) { // Iterate through features
-                _input_mask[i] = input12_t(inputs.candidate_inputs[16 + inputs.total_candidate_inputs*i]);
+                pt_mask[i] = input12_t(inputs.candidate_inputs[16 + inputs.total_candidate_inputs*i]);
             }
 
             // Jet features
             for (int i = 0; i < 2; ++i) { // Iterate through jet features
-                _input_jets[i] = input14_t(inputs.jet_inputs[i]);
+                jet_features[i] = input14_t(inputs.jet_inputs[i]);
             }
+        }
 
         virtual void predict()
         {
-            L1TSC4NGJetModel_v2_1_0(_input_basic, _input_fractions, _input_mask, _input_jets, _layer39_out, _layer40_out);
+            L1TSC4NGJetModel_v2_1_0(basic_input, constituent_fraction, jet_features, pt_mask, class_out, reg_out);
 
         }
 
@@ -69,11 +70,11 @@ class L1TSC4NGJetModel_emulator_v2_1_0 : public hls4mlEmulator::Model{
             auto *results_p = std::any_cast<ModelOutputs*>(result);
             // Basic output 1 regression output
             for (int i = 0; i < 1; ++i ){
-                results_p->jet_regression_output[i] = inputtype(_layer40_out[i]);
+                results_p->jet_regression_output[i] = inputtype(reg_out[i]);
             }
             // Basic output 8 class outputs
             for (int i = 0; i < 9; ++i ){
-                results_p->jet_class_output[i] = inputtype(_layer39_out[i]);
+                results_p->jet_class_output[i] = inputtype(class_out[i]);
             }
         }
 
--- setup.sh
+++ setup.sh
@@ -1,44 +1,43 @@
-VERSION=$1
-TARGET=L1TSC4NGJetModel_v2_0_0/NN
+MODEL_VERSION=$1
+WRAPPER_VERSION=$2
+TARGET=L1TSC4NGJetModel_$WRAPPER_VERSION/NN
 mkdir -p $TARGET
 SRC=firmware
 
 cp -r $SRC/defines.h ${TARGET}
-sed -i "s/#include <cstdio>/#include <cstdio>\n\nnamespace hls4ml_L1TSC4NGJetModel_$VERSION {/" ${TARGET}/defines.h
+sed -i "s/#include <cstdio>/#include <cstdio>\n\nnamespace hls4ml_L1TSC4NGJetModel_$MODEL_VERSION {/" ${TARGET}/defines.h
 echo '}' >> ${TARGET}/defines.h
 
-cp $SRC/L1TSC4NGJetModel.cpp   ${TARGET}/L1TSC4NGJetModel_$VERSION.cpp
-sed -i "s/void L1TSC4NGJetModel(/void L1TSC4NGJetModel_$VERSION(/" ${TARGET}/L1TSC4NGJetModel_$VERSION.cpp
-sed -i "s/void L1TSC4NGJetModel_${VERSION}(/namespace hls4ml_L1TSC4NGJetModel_$VERSION {\nvoid L1TSC4NGJetModel_$VERSION(/" ${TARGET}/L1TSC4NGJetModel_$VERSION.cpp
-echo '}' >> ${TARGET}/L1TSC4NGJetModel_$VERSION.cpp
-sed -i "s/L1TSC4NGJetModel.h/L1TSC4NGJetModel_$VERSION.h/" ${TARGET}/L1TSC4NGJetModel_$VERSION.cpp
+cp $SRC/L1TSC4NGJetModel.cpp   ${TARGET}/L1TSC4NGJetModel_$MODEL_VERSION.cpp
+sed -i "s/void L1TSC4NGJetModel(/void L1TSC4NGJetModel_$MODEL_VERSION(/" ${TARGET}/L1TSC4NGJetModel_$MODEL_VERSION.cpp
+sed -i "s/void L1TSC4NGJetModel_${MODEL_VERSION}(/namespace hls4ml_L1TSC4NGJetModel_$MODEL_VERSION {\nvoid L1TSC4NGJetModel_$MODEL_VERSION(/" ${TARGET}/L1TSC4NGJetModel_$MODEL_VERSION.cpp
+echo '}' >> ${TARGET}/L1TSC4NGJetModel_$MODEL_VERSION.cpp
+sed -i "s/L1TSC4NGJetModel.h/L1TSC4NGJetModel_$MODEL_VERSION.h/" ${TARGET}/L1TSC4NGJetModel_$MODEL_VERSION.cpp
 
-cp    $SRC/L1TSC4NGJetModel.h   ${TARGET}/L1TSC4NGJetModel_$VERSION.h
-sed -i "s/L1TSC4NGJetModel/L1TSC4NGJetModel_$VERSION/" ${TARGET}/L1TSC4NGJetModel_$VERSION.h
-sed -i "s/void L1TSC4NGJetModel_${VERSION}(/namespace hls4ml_L1TSC4NGJetModel_$VERSION {\nvoid L1TSC4NGJetModel_$VERSION(/" ${TARGET}/L1TSC4NGJetModel_$VERSION.h
-sed -i "s/#endif/}\n\n#endif/" ${TARGET}/L1TSC4NGJetModel_$VERSION.h
-sed -i "s/L1TSC4NGJETMODEL_H_/L1TSC4NGJETMODEL_${VERSION}_H_/" ${TARGET}/L1TSC4NGJetModel_$VERSION.h
+cp    $SRC/L1TSC4NGJetModel.h   ${TARGET}/L1TSC4NGJetModel_$MODEL_VERSION.h
+sed -i "s/L1TSC4NGJetModel/L1TSC4NGJetModel_$MODEL_VERSION/" ${TARGET}/L1TSC4NGJetModel_$MODEL_VERSION.h
+sed -i "s/void L1TSC4NGJetModel_${MODEL_VERSION}(/namespace hls4ml_L1TSC4NGJetModel_$MODEL_VERSION {\nvoid L1TSC4NGJetModel_$MODEL_VERSION(/" ${TARGET}/L1TSC4NGJetModel_$MODEL_VERSION.h
+sed -i "s/#endif/}\n\n#endif/" ${TARGET}/L1TSC4NGJetModel_$MODEL_VERSION.h
+sed -i "s/L1TSC4NGJETMODEL_H_/L1TSC4NGJETMODEL_${MODEL_VERSION}_H_/" ${TARGET}/L1TSC4NGJetModel_$MODEL_VERSION.h
 
 cp -r $SRC/nnet_utils ${TARGET}
 
 cp    $SRC/parameters.h ${TARGET}
-sed -i "s,// hls-fpga-machine-learning insert layer-config,namespace hls4ml_L1TSC4NGJetModel_$VERSION {\n// hls-fpga-machine-learning insert layer-config," ${TARGET}/parameters.h
+sed -i "s,// hls-fpga-machine-learning insert layer-config,namespace hls4ml_L1TSC4NGJetModel_$MODEL_VERSION {\n// hls-fpga-machine-learning insert layer-config," ${TARGET}/parameters.h
 sed -i "s/#endif/}\n\n#endif/" ${TARGET}/parameters.h
 
-mv ${TARGET}/../L1TSC4NGJetModel_emulator.cpp ${TARGET}/../L1TSC4NGJetModel_emulator_$VERSION.cpp
-sed -i "s/L1TSC4NGJetModel.h/L1TSC4NGJetModel_$VERSION.h/" ${TARGET}/../L1TSC4NGJetModel_emulator_$VERSION.cpp
-sed -i "s/hls4ml_L1TSC4NGJetModel/hls4ml_L1TSC4NGJetModel_$VERSION/" ${TARGET}/../L1TSC4NGJetModel_emulator_$VERSION.cpp
-sed -i "s/L1TSC4NGJetModel(/L1TSC4NGJetModel_$VERSION(/" ${TARGET}/../L1TSC4NGJetModel_emulator_$VERSION.cpp
+cp ${TARGET}/../L1TSC4NGJetModel_emulator_$WRAPPER_VERSION.cpp ${TARGET}/../L1TSC4NGJetModel_emulator_$MODEL_VERSION.cpp
+sed -i "s/${WRAPPER_VERSION}/${MODEL_VERSION}/" ${TARGET}/../L1TSC4NGJetModel_emulator_$MODEL_VERSION.cpp
 
-sed -i "s/L1TSC4NGJetModel.o/L1TSC4NGJetModel_$VERSION.o/" ${TARGET}/../Makefile
-sed -i "s/L1TSC4NGJetModel.so/L1TSC4NGJetModel_$VERSION.so/" ${TARGET}/../Makefile
-sed -i "s/L1TSC4NGJetModel_emulator.o/L1TSC4NGJetModel_emulator_$VERSION.o/" ${TARGET}/../Makefile
-sed -i "s,ALL_VERSIONS:=L1TSC4NGJetModel/L1TSC4NGJetModel.so,ALL_VERSIONS:=L1TSC4NGJetModel_$VERSION/L1TSC4NGJetModel_$VERSION.so," ${TARGET}/../../Makefile
+sed -i "s/L1TSC4NGJetModel_$WRAPPER_VERSION.o/L1TSC4NGJetModel_$MODEL_VERSION.o/" ${TARGET}/../Makefile
+sed -i "s/L1TSC4NGJetModel_$WRAPPER_VERSION.so/L1TSC4NGJetModel_$MODEL_VERSION.so/" ${TARGET}/../Makefile
+sed -i "s/L1TSC4NGJetModel_emulator_$WRAPPER_VERSION.o/L1TSC4NGJetModel_emulator_$MODEL_VERSION.o/" ${TARGET}/../Makefile
+sed -i "s,L1TSC4NGJetModel_$WRAPPER_VERSION/L1TSC4NGJetModel_$WRAPPER_VERSION.so,L1TSC4NGJetModel_$MODEL_VERSION/L1TSC4NGJetModel_$MODEL_VERSION.so," ${TARGET}/../../Makefile
 
 cp -r $SRC/weights ${TARGET}
 for filename in ${TARGET}/weights/*.h; do
     echo $filename
-    sed -i "8 a namespace hls4ml_L1TSC4NGJetModel_$VERSION {" $filename
+    sed -i "8 a namespace hls4ml_L1TSC4NGJetModel_$MODEL_VERSION {" $filename
     echo '}' >> $filename
 done
 
@@ -50,6 +49,6 @@ find ./${TARGET} \( -type d -name .git -prune \) -o -type f -print0 | xargs -0 s
 # change the way the weights are read
 find ./${TARGET} \( -type d -name .git -prune \) -o -type f -print0 | xargs -0 sed -i 's/#ifndef __SYNTHESIS__/#ifdef __HLS4ML_LOAD_TXT_WEIGHTS__/'
 
-mv L1TSC4NGJetModel L1TSC4NGJetModel_$VERSION
+cp -r L1TSC4NGJetModel_$WRAPPER_VERSION L1TSC4NGJetModel_$MODEL_VERSION
 
-rm -r $SRC
+#rm -r $SRC
